d57afeaba5ee04692a868c6899a04588d767cebd
cochanae-del <co.chanae@gmail.com>
2026-07-23 11:25:49 +0000

    Create function name countdown and prints blast off after 1.

--- main.js
+++ main.js
@@ -1,8 +1,11 @@
 // Write your code below
-function countDown () {
+function countDown() {
     for(i=10; i>=1; i--){
         console.log(i)
-    }
+        if (i === 1) {
+            console.log("Blast off!");
+        }
+    };
 }
 
 countDown()
