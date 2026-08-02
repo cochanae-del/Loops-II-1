7f827be1df59c0af2c10186c28fd600091f578f0
cochanae-del <co.chanae@gmail.com>
2026-08-02 00:56:38 +0000

    Create function  countdown, again. put in right space.

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 function countDown() {
     for(i=10; i>=1; i--){
         console.log(i)
-        if (i === 1) {
+        if (i === 0) {
             console.log("Blast off!");
-        }
+       }
     };
 }
 
 countDown()
+     console.log("Blast off!");
